589b3a1245afa77d486c3ad0ec7f76a95d9694c5
shir0areed <38010555+shir0areed@users.noreply.github.com>
2026-07-26 20:47:38 +0900

    start.py

---
+++ start.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+import pathlib
+import venv
+
+
+APP_MODULE = 'adira_resolver'
+
+
+if __name__ == '__main__':
+    script_dir = pathlib.Path(__file__).resolve().parent
+    venv_dir = script_dir / 'venv'
+
+    # python -m venv <venv_dir>
+    builder = venv.EnvBuilder(with_pip=True)
+    builder.create(str(venv_dir))
+    context = builder.ensure_directories(str(venv_dir))
+    venv_python = context.env_exe
+
+    # pip install -e .
+    subprocess.check_call(
+        [venv_python, '-m', 'pip', 'install', '-e', str(script_dir)],
+    )
+
+    # python -m <APP_MODULE> <args>
+    subprocess.check_call(
+        [venv_python, '-m', APP_MODULE] + sys.argv[1:]
+    )
